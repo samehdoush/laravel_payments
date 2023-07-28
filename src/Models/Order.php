35d6bb1cdfcab96da58361ab41b63da8a8998d21
sameh doush <samehdev95@gmail.com>
2023-07-28 20:31:46 +0300

    Update Order.php

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -4,12 +4,15 @@ namespace Samehdoush\LaravelPayments\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Samehdoush\LaravelPayments\Traits\BelongsToPlan;
+use Illuminate\Database\Eloquent\Builder;
 
 class Order extends Model
 {
     use HasFactory;
+    use Prunable;
     use BelongsToPlan;
     protected $guarded = [];
     public function __construct(array $attributes = [])
@@ -33,5 +36,12 @@ class Order extends Model
     public function orderable(): MorphTo
     {
         return $this->morphTo('orderable', 'orderable_type', 'orderable_id', 'id');
+    }
+
+    // remove order if created_at is older than 12 hours and Status is Waiting
+
+    public function prunable(): Builder
+    {
+        return static::where('status', 'Waiting')->where('created_at', '<', now()->subHours(12));
     }
 }
